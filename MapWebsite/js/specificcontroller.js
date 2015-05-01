b9456c46a0fd4bf26618d4c1d31d8d06c21792a0
22mano1bil <22mano1bil@gmail.com>
2015-05-01 14:16:27 +0200

    added navigation bar, added x3d example, bugfixed the upload errors

--- MapWebsite/js/specificcontroller.js
+++ MapWebsite/js/specificcontroller.js
@@ -3,29 +3,40 @@ $("#floorplanjson").fileinput({
     showUpload: false
 });
 $("#x3dmodel").fileinput({
-    allowedFileExtensions: ["json"],
+    allowedFileExtensions: ["x3d"],
     showUpload: false
 });
-
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 $('#submitFiles').click(function() {
-    var data = new FormData();
-    var file = $('#floorplanjson')[0].files;
-    data.append('floorplanjson', file[0]);
-    var file = $('#x3dmodel')[0].files;
-    data.append('x3dmodel', file[0]);
-    $.ajax({
-        url: '/api/photo',
-        type: 'POST',
-        data: data,
-        async: false,
-        cache: false,
-        contentType: false,
-        enctype: 'multipart/form-data',
-        processData: false,
-        success: function(response) {
-            console.log(response);
+    if ((typeof $('#floorplanjson')[0].files[0].name !== 'undefined') &&
+            endsWith($('#floorplanjson')[0].files[0].name, '.json')) {
+        var data = new FormData();
+
+        var floorplanjson = $('#floorplanjson')[0].files;
+        data.append('floorplanjson', floorplanjson[0]);
+
+        if ((typeof $('#x3dmodel')[0].files[0].name !== 'undefined') &&
+                endsWith($('#x3dmodel')[0].files[0].name, '.x3d')) {
+            var x3dmodel = $('#x3dmodel')[0].files;
+            data.append('x3dmodel', x3dmodel[0]);
         }
-    });
+
+        $.ajax({
+            url: '/api/photo',
+            type: 'POST',
+            data: data,
+            async: false,
+            cache: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            processData: false,
+            success: function(response) {
+            console.log(response);
+            }
+        });
+    }
     return false;
 });
 

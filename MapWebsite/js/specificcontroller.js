4614ee7e084f4986b1a14b941b118f058774541e
22mano1bil <22mano1bil@gmail.com>
2015-05-10 18:43:26 +0200

    view for create or watch a szenario

--- MapWebsite/js/specificcontroller.js
+++ MapWebsite/js/specificcontroller.js
@@ -9,33 +9,53 @@ $("#x3dmodel").fileinput({
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
+
+$('#sensorcomparison #aggregation .active input').val();
+
+$('#createwatch').click(function() { 
+    var lastmodi = $(this).find('.active input').val();
+    if (lastmodi=='create'){
+    $('#create').hide();
+    $('#watch').show();
+    }
+    if (lastmodi=='watch'){
+    $('#watch').hide();
+    $('#create').show();
+    }    
+});
+
+
+
+/*deprecated since upload isn't working anymore with express.io*/
 $('#submitFiles').click(function() {
     if ((typeof $('#floorplanjson')[0].files[0].name !== 'undefined') &&
             endsWith($('#floorplanjson')[0].files[0].name, '.json')) {
-        var data = new FormData();
-
-        var floorplanjson = $('#floorplanjson')[0].files;
-        data.append('floorplanjson', floorplanjson[0]);
-
-        if ((typeof $('#x3dmodel')[0].files[0].name !== 'undefined') &&
-                endsWith($('#x3dmodel')[0].files[0].name, '.x3d')) {
-            var x3dmodel = $('#x3dmodel')[0].files;
-            data.append('x3dmodel', x3dmodel[0]);
-        }
-
-        $.ajax({
-            url: '/api/photo',
-            type: 'POST',
-            data: data,
-            async: false,
-            cache: false,
-            contentType: false,
-            enctype: 'multipart/form-data',
-            processData: false,
-            success: function(response) {
-            console.log(response);
-            }
-        });
+        upload();
+//        var data = new FormData();
+//
+//        var floorplanjson = $('#floorplanjson')[0].files;
+//        data.append('floorplanjson', floorplanjson[0]);
+//
+//        if ((typeof $('#x3dmodel')[0].files[0].name !== 'undefined') &&
+//                endsWith($('#x3dmodel')[0].files[0].name, '.x3d')) {
+//            var x3dmodel = $('#x3dmodel')[0].files;
+//            data.append('x3dmodel', x3dmodel[0]);
+//        }
+//
+//        $.ajax({
+//            url: '/api/photo',
+//            type: 'POST',
+//            data: data,
+//            async: true,
+//            cache: false,
+//            contentType: false,
+//            enctype: 'multipart/form-data',
+//            processData: false,
+//            success: function(response) {
+//            console.log(response);
+//            }
+//        });
+//        console.log('Uploaded');
     }
     return false;
 });
@@ -63,3 +83,26 @@ $('#submitFloormapJSON').click(function() {
     return false;
 });
 
+   var client = new XMLHttpRequest();
+  
+   function upload() 
+   {
+        var data = new FormData();
+
+        var floorplanjson = $('#floorplanjson')[0].files;
+        data.append('floorplanjson', floorplanjson[0]);
+        console.log(floorplanjson);
+
+      client.open("post", "/api/photo", true);
+      client.setRequestHeader("Content-Type", "multipart/form-data");
+      client.send(data);  /* Send to server */ 
+   }
+     
+   /* Check the response status */  
+//   client.onreadystatechange = function() 
+//   {
+//      if (client.readyState == 4 && client.status == 200) 
+//      {
+//         alert(client.statusText);
+//      }
+//   }
--- webserver/webserver.js
+++ webserver/webserver.js
@@ -50,28 +50,29 @@ app.post('/newPath', function(req, res) {
 
 
 
-/*Define dependencies.*/
-var multer = require('multer');
-var done = false;
-
-/*Configure the multer.*/
-app.use(multer({dest: './uploads/',
-    rename: function(fieldname, filename) {
-        return filename + Date.now();
-    },
-    onFileUploadStart: function(file) {
-        console.log(file.originalname + ' is starting ...')
-    },
-    onFileUploadComplete: function(file) {
-        console.log(file.fieldname + ' uploaded to  ' + file.path)
-        done = true;
-    }
-}));
-
-/*Handling routes.*/
-app.post('/api/photo', function(req, res) {
-    if (done == true) {
-        console.log(req.files);
-        res.end("File uploaded.");
-    }
-});
+/*Define dependencies. its not working anymore since express.io */
+//var multer = require('multer');
+//var done = false;
+//
+///*Configure the multer.*/
+//app.use(multer({dest: './uploads/',
+//    rename: function(fieldname, filename) {
+//        return filename + Date.now();
+//    },
+//    onFileUploadStart: function(file) {
+//        console.log(file.originalname + ' is starting ...')
+//    },
+//    onFileUploadComplete: function(file) {
+//        console.log(file.fieldname + ' uploaded to  ' + file.path)
+//        done = true;
+//    }
+//}));
+//
+///*Handling routes.*/
+//app.post('/api/photo', function(req, res) {
+//    console.log("getRequest upload");
+//    if (done == true) {
+//        console.log(req.files);
+//        res.end("File uploaded.");
+//    }
+//});

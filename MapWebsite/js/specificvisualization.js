888bb98126cd615b128c7ed691234c380c336bd4
22mano1bil <22mano1bil@gmail.com>
2015-05-14 14:08:33 +0200

    shows actual position of roboter as a zylinder, shows newPath als zylinders and spheres, shows external x3D model with inline element on szenario submit; added screenshots

--- MapWebsite/js/specificvisualization.js
+++ MapWebsite/js/specificvisualization.js
@@ -13,9 +13,181 @@ io.on('szenario', function (data) {
 });
 
 function buildNewSzenario(data) {
-    $('X3D scene').append(zylinderExample());
+    $('X3D scene').append(addx3dModel(data));
+    $('X3D scene').append(x3dExample());
 }
-function zylinderExample() {
+
+function addx3dModel(data) {
+//    return "<inline url="+data['modelX3DREF']+"></inline>";
+    return "<inline url=Deer.x3d></inline>";
+}
+
+function sphereWithPoint(p) {
+//Berechnung der Attribute des Bindungszylinders
+    var point = {};
+    point.x = p.x;
+    point.y = p.y;
+    point.z = 0;
+    
+    var pointString = point.x + " "+ point.y+" "+ point.z;    
+    var sphereString = 
+            "<Transform translation='"+pointString+"'>" +
+            "<Shape>" +
+            "<Sphere radius='0.2'/>" +
+            "<Appearance>" +
+            "<Material diffuseColor='0.1 0.7 0.3'/>" +
+            "</Appearance>" +
+            "</Shape>" +
+            "</Transform>";
+    console.log(sphereString);
+    $('X3D scene').append(sphereString);
+    return sphereString;
+};
+
+function zylinderWithMiddlePoint(mp) {
+//Berechnung der Attribute des Bindungszylinders
+    var point1 = {};
+    point1.x = mp.x;
+    point1.y = mp.y;
+    point1.z = 0;
+
+    var point2 = {};
+    point2.x = mp.x;
+    point2.y = mp.y;
+    point2.z = 0.21;
+
+// Vektor zwischen den Atomen
+    var vector = {};
+    vector.x = (point2.x - point1.x);
+    vector.y = (point2.y - point1.y);
+    vector.z = (point2.z - point1.z);
+    
+    var length = Math.sqrt(vector.x*vector.x + vector.y * vector.y + vector.z * vector.z);
+    console.log(length);
+    
+// halbierter Vektor zwischen den Atomen
+    var middle = {};
+    middle.x = vector.x / 2;
+    middle.y = vector.y / 2;
+    middle.z = vector.z / 2;
+
+// Mittelpunkt zwischen den beiden Punkten
+    var position = (point1.x + middle.x) + " "
+            + (point1.y + middle.y) + " " + (point1.z + middle.z);
+
+// Winkel zwischen middle und Y-Achse
+    var help = middle.x * middle.x + middle.y * middle.y
+            + middle.z * middle.z;
+    var angle = Math.acos(middle.y / Math.sqrt(help));
+
+// Bestimmung der Rotationsachse
+    var rotation;
+    if (middle.x == 0) {
+        rotation = "1 0 0 ";
+        // falls notwendig "richtig" herum drehen
+        if (middle.z < 0)
+            angle = (2 * Math.PI) - angle;
+    }
+    else {
+        rotation = -middle.z / middle.x + " 0 1 ";
+        // falls notwendig "richtig" herum drehen
+        if (middle.x > 0)
+            angle = (2 * Math.PI) - angle;
+    }
+// Angabe einer Rotation in X3D:
+// 3 Koordinaten der Rotationsachse + Winkel
+    rotation = rotation + angle;
+
+
+    var zylinderString = 
+            "<Transform translation='" + position + "'>" + //z is always null/static
+            "<Transform rotation='" + rotation + "'>" +
+            "<Shape>" +
+            "<Cylinder height='"+length+"' radius='0.4' />" + //radius and heigt is static
+            "<Appearance>" +
+            "<Material diffuseColor='0.5 0.5 1' />" + //color is static
+            "</Appearance>" +
+            "</Shape>" +
+            "</Transform>" +
+            "</Transform>";
+    console.log(zylinderString);
+    $('X3D scene').append(zylinderString);
+    return zylinderString;
+};
+
+function zylinderWithStartpointAndEndpoint(p1, p2) {
+//Berechnung der Attribute des Bindungszylinders
+    var point1 = {};
+    point1.x = p1.x;
+    point1.y = p1.y;
+    point1.z = 0;
+
+    var point2 = {};
+    point2.x = p2.x;
+    point2.y = p2.y;
+    point2.z = 0;
+
+// Vektor zwischen den Atomen
+    var vector = {};
+    vector.x = (point2.x - point1.x);
+    vector.y = (point2.y - point1.y);
+    vector.z = (point2.z - point1.z);
+    
+    var length = Math.sqrt(vector.x*vector.x + vector.y * vector.y + vector.z * vector.z);
+    console.log(length);
+    
+// halbierter Vektor zwischen den Atomen
+    var middle = {};
+    middle.x = vector.x / 2;
+    middle.y = vector.y / 2;
+    middle.z = vector.z / 2;
+
+// Mittelpunkt zwischen den beiden Punkten
+    var position = (point1.x + middle.x) + " "
+            + (point1.y + middle.y) + " " + (point1.z + middle.z);
+
+// Winkel zwischen middle und Y-Achse
+    var help = middle.x * middle.x + middle.y * middle.y
+            + middle.z * middle.z;
+    var angle = Math.acos(middle.y / Math.sqrt(help));
+
+// Bestimmung der Rotationsachse
+    var rotation;
+    if (middle.x == 0) {
+        rotation = "1 0 0 ";
+        // falls notwendig "richtig" herum drehen
+        if (middle.z < 0)
+            angle = (2 * Math.PI) - angle;
+    }
+    else {
+        rotation = -middle.z / middle.x + " 0 1 ";
+        // falls notwendig "richtig" herum drehen
+        if (middle.x > 0)
+            angle = (2 * Math.PI) - angle;
+    }
+// Angabe einer Rotation in X3D:
+// 3 Koordinaten der Rotationsachse + Winkel
+    rotation = rotation + angle;
+
+
+    var zylinderString = 
+            "<Transform translation='" + position + "'>" + //z is always null/static
+            "<Transform rotation='" + rotation + "'>" +
+            "<Shape>" +
+            "<Cylinder height='"+length+"' radius='0.1' />" + //radius and heigt is static
+            "<Appearance>" +
+            "<Material diffuseColor='0.5 0.5 0.5' />" + //color is static
+            "</Appearance>" +
+            "</Shape>" +
+            "</Transform>" +
+            "</Transform>";
+    console.log(zylinderString);
+    $('X3D scene').append(zylinderString);
+    return zylinderString;
+};
+
+
+function x3dExample() {
 
 //Berechnung der Attribute des Bindungszylinders
     var point1 = {};
@@ -71,7 +243,8 @@ function zylinderExample() {
     rotation = rotation + angle;
 
 
-    var string = "<Transform translation='" + position + "'>" + //z is always null/static
+    var string = 
+            "<Transform translation='" + position + "'>" + //z is always null/static
             "<Transform rotation='" + rotation + "'>" +
             "<Shape>" +
             "<Cylinder height='"+length+"' radius='0.1' />" + //radius and heigt is static
@@ -144,6 +317,11 @@ io.on('actualPosition', function (data) {
 io.on('actualPositionArray', function (data) {
     console.log('actualPosition');
     console.log(data);
+    $.each(data, function(i, v) {
+        console.log(v);
+        var actualPosition = v['actualPosition'];
+        zylinderWithMiddlePoint(actualPosition['point']);
+    });  
 })
 
 // Listen for the newPath event.
@@ -156,4 +334,16 @@ io.on('newPath', function (data) {
 io.on('newPathArray', function (data) {
     console.log('newPathArray');
     console.log(data);
-})
+    $.each(data, function(i, v) {
+        console.log(v);
+        var newPath = v['newPath'];
+        sphereWithPoint(newPath[0]);
+        if(newPath.length>1){
+            for (i = 0; i < newPath.length-1; i++) { 
+                //console.log(newPath[i]);
+                zylinderWithStartpointAndEndpoint(newPath[i],newPath[i+1]);
+                sphereWithPoint(newPath[i+1]);
+            }
+        }
+    });  
+});
